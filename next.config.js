e2ba7f00fd484c3a626d7e41f712dd740eb05170
TMC <the-mushroom-colonel@Thes-iMac.local>
2022-11-10 15:20:09 -0800

    second attempt at fixing image src

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: "./",
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns:[{
+        protocol:'https',
+        hostname:'tmb.imgix.net'
+      }]
+    },
 };
 
 module.exports = nextConfig;
