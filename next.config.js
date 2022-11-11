6beb0f26e5093cdbcc452224a206383637eb665d
TMC <the-mushroom-colonel@Thes-iMac.local>
2022-11-11 13:10:47 -0800

    trying again to get ipfs images working

--- next.config.js
+++ next.config.js
@@ -7,15 +7,19 @@ const nextConfig = {
     remotePatterns:[{
         protocol:'https',
         hostname:'tmb.imgix.net'
-      }, {
-        protocol: 'https',
-        hostname: 'bafkreihjag3e6uz3ykuzjfsu6qlvyejznug5maz3z2hha3a2ufo5xhxjxi.ipfs.nftstorage.link'
       }
-      , {
-        protocol: 'https',
-        hostname: 'bafkreid5yasqlpflxg2fq7irzs5mgisvq7waskc5t2tf7nlrkqtaeaj3wm.ipfs.nftstorage.link'
-      }
-    ]
+      // , {
+      //   protocol: 'https',
+      //   hostname: 'nftstorage',
+      //   pathname: '**'
+      // }
+      // , {
+      //   protocol: 'https',
+      //   hostname: 'bafkreid5yasqlpflxg2fq7irzs5mgisvq7waskc5t2tf7nlrkqtaeaj3wm',
+      //   pathname: '**'
+      // }
+    ],
+    domains:['bafkreihjag3e6uz3ykuzjfsu6qlvyejznug5maz3z2hha3a2ufo5xhxjxi.ipfs.nftstorage.link', 'bafkreid5yasqlpflxg2fq7irzs5mgisvq7waskc5t2tf7nlrkqtaeaj3wm.ipfs.nftstorage.link']
     },
 };
 
