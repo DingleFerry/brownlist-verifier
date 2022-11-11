1fbd5031084f9343a6e1607c02670561d2eca262
TMC <the-mushroom-colonel@Thes-iMac.local>
2022-11-11 14:30:11 -0800

    another stab at getting IPFS images working

--- next.config.js
+++ next.config.js
@@ -30,8 +30,8 @@ const nextConfig = {
       //   pathname: '**'
       // }
     ]
-    // ,
-    // domains:['bafkreihjag3e6uz3ykuzjfsu6qlvyejznug5maz3z2hha3a2ufo5xhxjxi.ipfs.nftstorage.link', 'bafkreid5yasqlpflxg2fq7irzs5mgisvq7waskc5t2tf7nlrkqtaeaj3wm.ipfs.nftstorage.link']
+    ,
+    domains:['bafkreihjag3e6uz3ykuzjfsu6qlvyejznug5maz3z2hha3a2ufo5xhxjxi.ipfs.nftstorage.link', 'bafkreid5yasqlpflxg2fq7irzs5mgisvq7waskc5t2tf7nlrkqtaeaj3wm.ipfs.nftstorage.link']
     },
 };
 
