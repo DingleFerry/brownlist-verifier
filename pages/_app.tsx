029dfa5cf0e41f231cce2721f97dd1d6e4af293d
TMC <the-mushroom-colonel@Thes-iMac.local>
2022-11-21 19:51:50 -0800

    attempt 1 at installing Goog anals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,53 @@ import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
+import { useEffect } from "react";
+
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mainnet;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+
+// const App = ({ Component, pageProps }: AppProps) => {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const handleRouteChange = (url: URL) => {
+//       gtag.pageview(url);
+//     };
+//     router.events.on("routeChangeComplete", handleRouteChange);
+//     return () => {
+//       router.events.off("routeChangeComplete", handleRouteChange);
+//     };
+//   }, [router.events]);
+
+//   return <Component {...pageProps} />;
+// };
+
+// export default App;
